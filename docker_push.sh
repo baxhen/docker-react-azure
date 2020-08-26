faf342d1cd24c0f7ba56b680d29be27c51ffb34c
Leonardo Rebouças Batista <leo292629@gmail.com>
2020-08-26 01:29:49 -0300

    deploy azure 18

--- docker_push.sh
+++ docker_push.sh
@@ -1,2 +1,3 @@
-docker login --username "$AZURE_USERNAME" --password "$AZURE_PASSWORD"
+echo "$AZURE_PASSWORD" | docker login -u "$AZURE_USERNAME" --password-stdin
 docker push dockerbaxhen.azurecr.io/azure-docker;
+
