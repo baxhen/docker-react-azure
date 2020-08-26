912bdecdb686d25dc7b3416aaca11b90fb8a4fac
Leonardo Rebouças Batista <leo292629@gmail.com>
2020-08-26 10:54:56 -0300

    chenged app text

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,4 @@
 echo "$AZURE_PASSWORD" | docker login -u "$AZURE_WA_USERNAME" --password-stdin "$AZURE_LOGIN_URL"
+docker build -t dockerbaxhen.azurecr.io/azure-docker -f Dockerfile .
 docker push dockerbaxhen.azurecr.io/azure-docker;
 
